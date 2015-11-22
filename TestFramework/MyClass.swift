cd97bc65eea2df72c6a603548863a12068247bbd
glr <richard.gal@pocketguideapp.com>
2015-11-22 08:51:01 +0100

    Added class

---
+++ TestFramework/MyClass.swift
@@ -0,0 +1,14 @@
+//
+//  MyClass.swift
+//  TestFramework
+//
+//  Created by Gal Richard on 22/11/15.
+//  Copyright © 2015 PocketGuide. All rights reserved.
+//
+
+import UIKit
+
+class MyClass: NSObject {
+
+    let name: String = "Hello from TestFramework"
+}
